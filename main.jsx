80b3d5c80edaeba18849b5d2cb48419b4df8f101
alafbulgari-debug <alaf.bulgari@gmail.com>
2026-06-13 13:22:20 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://hkmylkafhknknewsrbea.supabase.co';
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_jzpaMXj7gwmJibb6wb-paw_bNqxu6tI';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
